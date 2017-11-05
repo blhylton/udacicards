756feb543483632352011ad79371f70d0699e22e
Barry Hylton <blhylton@blhylton.com>
2017-11-05 12:34:39 -0500

    Add DeckTitle Screen

--- components/DeckTitle.js
+++ components/DeckTitle.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react'
 import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
+import { getDeck } from '../util/helpers'
 
 export default class DeckTitle extends Component {
+    constructor(props){
+        super(props)
+        this.state = {deck: {questions: []}}
+    }
+
+    componentDidMount = () => {
+        getDeck(this.props.deck).then(deck => this.setState(
+            {
+                deck
+            }
+        ))
+    }
     render() {
+        const {deck} = this.state
         return (
             <View>
-                <Text>Deck Title</Text>
+                <Text>{deck.title}</Text>
+                <Text>{deck.questions.length}</Text>
+                <TouchableOpacity>
+                    <Text>Add Card</Text>
+                </TouchableOpacity>
+                <TouchableOpacity>
+                    <Text>Start Quiz</Text>
+                </TouchableOpacity>
             </View>
         )
     }
