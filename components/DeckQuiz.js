7b8f799e859b5ecd5e1a256fbcc8e3e23d5287bb
Barry Hylton <blhylton@blhylton.com>
2017-11-05 13:54:57 -0500

    Style Quiz

--- components/DeckQuiz.js
+++ components/DeckQuiz.js
@@ -5,36 +5,95 @@ import QuizQuestion from './QuizQuestion'
 export default class DeckQuiz extends Component {
     constructor(props){
         super(props)
-        this.state = { score: 0, idx: 0 }
+        this.state = { score: 0, idx: 0, showAnswer: false }
+    }
+    flipCard = () => {
+        this.setState({
+            showAnswer: !this.state.showAnswer
+        })
     }
     render() {
         const { questions } = this.props
-        const { idx, score } = this.state
+        const { idx, score, showAnswer } = this.state
         return (
-            <View>
+            <View style={styles.container}>
                 {idx < questions.length ? (    
-                    <View>
-                        <Text>{idx + 1}/{questions.length}</Text>
-                        <QuizQuestion question={questions[idx]} />
-                        <TouchableOpacity onPress={() => this.setState({idx: idx + 1, score: score + 1})}>
-                            <Text>Correct</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity onPress={() => this.setState({idx: idx + 1})}>
-                            <Text>Incorrect</Text>
-                        </TouchableOpacity>
+                    <View style={{flex: 1}}>
+                        <Text style={styles.counter}>{idx + 1}/{questions.length}</Text>
+                        <QuizQuestion question={questions[idx]} showAnswer={showAnswer} flipCard={this.flipCard} style={{flex: 4}} />
+                        <View style={{flex: 2}}>
+                            <TouchableOpacity onPress={() => this.setState({idx: idx + 1, score: score + 1})} style={[styles.btn, styles.correctBtn]}>
+                                <Text style={styles.btnText}>Correct</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity onPress={() => this.setState({idx: idx + 1})} style={[styles.btn, styles.incorrectBtn]}>
+                                <Text style={styles.btnText}>Incorrect</Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
                 ) : (
-                    <View>
-                        <Text>{score/questions.length * 100 }% Correct</Text>
-                        <TouchableOpacity onPress={() => this.setState({idx: 0, score: 0})}>
-                            <Text>Try Again</Text>
-                        </TouchableOpacity>
-                        <TouchableOpacity>
-                            <Text>Back to List</Text>
-                        </TouchableOpacity>
+                    <View style={{flex: 1}}>
+                        <View style={styles.result}>
+                            <Text style={styles.resultText}>{score/questions.length * 100 }% Correct</Text>
+                        </View>
+                        <View style={{flex: 1}}>
+                            <TouchableOpacity onPress={() => this.setState({idx: 0, score: 0})} style={[styles.btn, styles.genericBtn]}>
+                                <Text style={styles.btnText}>Try Again</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={[styles.btn, styles.genericBtn]}>
+                                <Text style={styles.btnText}>Back to List</Text>
+                            </TouchableOpacity>
+                        </View>
                     </View>
                 )}
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginTop: 50,
+        alignSelf: 'stretch',
+        padding: 20
+    },
+    counter: {
+        fontSize: 20,
+        color: '#999999',
+        flex: 1,
+        alignItems: 'flex-start',
+        justifyContent: 'center'
+    },
+    result: {
+        flex: 2,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    resultText: {
+        fontSize: 28
+    },
+    btn: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 10,
+        padding: 15,
+        paddingLeft: 45,
+        paddingRight: 45,
+        alignSelf: 'stretch'
+    },
+    correctBtn:{
+        backgroundColor: 'green'
+    },
+    incorrectBtn: {
+        backgroundColor: 'red'
+    },
+    genericBtn: {
+        backgroundColor: 'gray'
+    },
+    btnText: {
+        color: 'white',
+        fontSize: 18
+    }
+})
--- components/QuizQuestion.js
+++ components/QuizQuestion.js
@@ -2,29 +2,44 @@ import React, { Component } from 'react'
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
 
 export default class QuizQuestion extends Component {
-    constructor(props){
-        super(props)
-        this.state = { showAnswer: false }
-    }
 
     render() {
-        const {question} = this.props
-        return this.state.showAnswer ? 
-                (
-                    <View>
-                        <Text>{question.answer}</Text>
-                        <TouchableOpacity onPress={() => this.setState({showAnswer: false})}>
-                            <Text>Show Question</Text>
-                        </TouchableOpacity>
-                    </View>
-                ) : 
-                (
-                    <View>
-                        <Text>{question.question}</Text>
-                        <TouchableOpacity onPress ={() => this.setState({showAnswer: true})}>
-                            <Text>Show Answer</Text>
-                        </TouchableOpacity>
-                    </View>
-                )
+        const {question, showAnswer, flipCard } = this.props
+        return showAnswer ? 
+            (
+                <View style={[this.props.style]}>
+                    <Text style={styles.question}>{question.answer}</Text>
+                    <TouchableOpacity onPress={() => flipCard()} style={styles.flipCard}>
+                        <Text style={styles.flipCardText}>Show Question</Text>
+                    </TouchableOpacity>
+                </View>
+            ) : 
+            (
+                <View style={[styles.container, this.props.style]}>
+                    <Text style={styles.question}>{question.question}</Text>
+                    <TouchableOpacity onPress ={() => flipCard()} style={styles.flipCard}>
+                        <Text style={styles.flipCardText}>Show Answer</Text>
+                    </TouchableOpacity>
+                </View>
+            )
     }
 }
+
+const styles = StyleSheet.create({
+    question: {
+        fontSize: 28,
+        margin: 20
+    },
+    flipCard: {
+        backgroundColor: 'white',
+        padding: 10,
+        paddingLeft: 40,
+        paddingRight: 40,
+        borderWidth: 1,
+        borderColor: 'gray',
+        alignItems: 'center'
+    },
+    flipCardText: {
+        color: 'red'
+    }
+})
