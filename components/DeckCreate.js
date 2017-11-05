90ec03e26c9f8f1edfbdf2b9dee2f76b262550c7
Barry Hylton <blhylton@blhylton.com>
2017-11-05 14:54:09 -0500

    Style DeckCreate

--- components/DeckCreate.js
+++ components/DeckCreate.js
@@ -21,22 +21,56 @@ class DeckCreate extends Component {
 
     render() {
         return (
-            <View>
-                <Text>What is the title of your new deck?</Text>
+            <View style={styles.container}>
+                <Text style={styles.label}>What is the title of your new deck?</Text>
                 <TextInput
                     onChangeText={(text) => this.setState({text, error: {status: false, text: ''}})}
                     value={this.state.text}
                     placeholder="Deck Title"
+                    style={styles.input}
                 />
                 {this.state.error.status && (
                     <Text>{this.state.error.text}</Text>
                 )}
-                <TouchableOpacity onPress={this.submit}>
-                    <Text>Submit</Text>
+                <TouchableOpacity onPress={this.submit} style={styles.submitBtn}>
+                    <Text style={styles.submitBtnText}>Submit</Text>
                 </TouchableOpacity>
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    label: {
+        fontSize: 30,
+        margin: 15,
+        marginTop: 0,
+        marginBottom: 20,
+        textAlign: 'center'
+    },
+    input: {
+        borderWidth: 1,
+        borderColor: '#333',
+        borderRadius: 5,
+        padding: 10,
+        alignSelf: 'stretch',
+        margin: 15
+    },
+    submitBtn: {
+        backgroundColor: '#333',
+        borderRadius: 5,
+        padding: 10,
+        paddingLeft: 40,
+        paddingRight: 40
+    },
+    submitBtnText: {
+        color: '#fff'
+    }
+})
 
 export default connect()(DeckCreate)
