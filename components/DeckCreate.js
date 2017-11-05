572c11904884d8e698e58118fbd275e6e6c66609
Barry Hylton <blhylton@blhylton.com>
2017-11-05 11:51:29 -0500

    Add ability to create deck

--- components/DeckCreate.js
+++ components/DeckCreate.js
@@ -1,12 +1,42 @@
 import React, { Component } from 'react'
-import {View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import {View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native'
+import { saveDeckTitle } from '../util/helpers'
+import { addDeck } from '../actions'
+import { connect } from 'react-redux'
 
-export default class DeckCreate extends Component {
+class DeckCreate extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { text: '', error: { status: false, text: ''} };
+    }
+    submit = () => {
+        if(this.state.text !== ''){
+            saveDeckTitle(this.state.text)
+                .then(this.props.dispatch(addDeck(this.state.text)))
+                .then(this.setState({text: '', error: { status: false, text: ''}}))
+        }else{
+            this.setState({error: {status: true, text: 'Title can not be empty'}})
+        }
+    }
+
     render() {
         return (
             <View>
-                <Text>Deck Create</Text>
+                <Text>What is the title of your new deck?</Text>
+                <TextInput
+                    onChangeText={(text) => this.setState({text, error: {status: false, text: ''}})}
+                    value={this.state.text}
+                    placeholder="Deck Title"
+                />
+                {this.state.error.status && (
+                    <Text>{this.state.error.text}</Text>
+                )}
+                <TouchableOpacity onPress={this.submit}>
+                    <Text>Submit</Text>
+                </TouchableOpacity>
             </View>
         )
     }
 }
+
+export default connect()(DeckCreate)
